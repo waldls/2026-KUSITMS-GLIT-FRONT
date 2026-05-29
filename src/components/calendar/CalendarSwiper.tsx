18b735403ab29847d80ddf39748b94e76df02a2a
waldls <p353700@gmail.com>
2026-05-29 17:31:47 +0000

    [MOD] height auto 속성 추가

--- src/components/calendar/CalendarSwiper.tsx
+++ src/components/calendar/CalendarSwiper.tsx
@@ -55,7 +55,7 @@ const CalendarSwiper = ({
     <>
       <Swiper initialSlide={1} speed={250} onTransitionEnd={handleTransitionEnd}>
         {slideMonths.map((month, i) => (
-          <SwiperSlide key={i}>
+          <SwiperSlide key={i} style={{ height: "auto" }}>
             <Calendar
               type="page"
               mode="single"
