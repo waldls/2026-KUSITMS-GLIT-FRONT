da7df5d320f294d13aa6d21fc88db5360add12e6
waldls <p353700@gmail.com>
2026-06-04 12:16:36 +0000

    [MOD] 커리어 리포트 패턴 텍스트 한국어 단어 단위 줄바꿈 수정

--- src/containers/report/career/PatternSection.tsx
+++ src/containers/report/career/PatternSection.tsx
@@ -5,7 +5,7 @@ interface PatternSectionProps {
 const PatternSection = ({ pattern }: PatternSectionProps) => {
   return (
     <div className="border-linear-100 rounded-8 px-10 py-5">
-      <p className="body-2 mx-auto max-w-43.75 text-center text-gray-100">{pattern}</p>
+      <p className="body-2 mx-auto max-w-43.75 text-center break-keep text-gray-100">{pattern}</p>
     </div>
   );
 };
